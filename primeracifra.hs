24dce22da04f5be5c269687a129950da98dbb0ec
Alejandro Chávez <82726929+Donakeru@users.noreply.github.com>
2024-09-16 16:45:57 -0500

    (UPDATE) prueba del moodle

--- primeracifra.hs
+++ primeracifra.hs
@@ -5,9 +5,7 @@ primeraCifra x
     | abs x < 10 = abs x  -- Daniel: Caso: 1Si el número es de un solo dígito, ese es el resultado
     | otherwise = primeraCifra (x `div` 10)  -- Daniel: Caso 2: Dividimos el número entre 10 hasta obtener un solo dígito
 
+-- prueba puesta por el profesor
 main = do
-    print (primeraCifra 325)  -- Resultado: 3
-    print (primeraCifra 789)  -- Resultado: 7
-    print (primeraCifra 42)   -- Resultado: 4
-    print (primeraCifra (-953))  -- Resultado: 9
+    print (primeraCifra 325)  -- 3
 
